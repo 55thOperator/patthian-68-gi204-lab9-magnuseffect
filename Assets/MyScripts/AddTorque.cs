c671c852621da011b0ad362ca2191f1c4bc6f883
55thOperator <89891897+55thOperator@users.noreply.github.com>
2026-03-10 10:22:16 +0700

    Built Soccer field

--- Assets/MyScripts/AddTorque.cs
+++ Assets/MyScripts/AddTorque.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AddTorque : MonoBehaviour
 {
+    Rigidbody rb;
+    public float spinSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddTorque(Vector3.forward * spinSpeed);
+        }
     }
 }
